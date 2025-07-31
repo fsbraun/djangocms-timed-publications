dd706a3ee14d8093200fa80c3005d9615dde412f
Fabian Braun <fsbraun@gmx.de>
2025-07-31 16:50:34 +0200

    tests: Update view tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,7 @@
 import pytest
 
 from cms.utils.urlutils import admin_reverse
+from djangocms_versioning.constants import DRAFT, PUBLISHED
 
 
 @pytest.mark.django_db
@@ -13,6 +14,7 @@ class TestToolbar:
         content = response.content.decode()
 
         assert content == "This view only supports GET or POST method."
+        assert page_content.versions.first().state == DRAFT
 
     def test_publish_renders_form_on_get(self, client, admin_user, page_content):
         version = page_content.versions.first()
@@ -25,6 +27,8 @@ class TestToolbar:
         assert "<b>Visible after</b>" in content
         assert "<b>Visible until</b>" in content
         
+        assert page_content.versions.first().state == DRAFT  # Just showed the form, no state change
+
     def test_publish_renders_form_errors(self, client, admin_user, page_content):
         version = page_content.versions.first()
         url = admin_reverse("djangocms_versioning_pagecontentversion_publish", args=(version.pk,))
@@ -37,13 +41,49 @@ class TestToolbar:
             '<ul class="errorlist" id="id_visibility_start_error"><li>Enter a valid time.</li></ul>' in content or 
             '<ul class="errorlist"><li>Enter a valid time.</li></ul>' in content  # Django 4.2
         )
+        assert page_content.versions.first().state == DRAFT  # Error, no state change
+
+
+    def test_publish_must_affect_future(self, client, admin_user, page_content, past_datetime):
+      
+        version = page_content.versions.first()
+        url = admin_reverse("djangocms_versioning_pagecontentversion_publish", args=(version.pk,))
+        client.login(username=admin_user.username, password='admin123')
+        data = {
+            "visibility_start_0": past_datetime.date().isoformat(),
+            "visibility_start_1": past_datetime.strftime("%H:%M"),
+        }
+        expected_error = "The date and time must be in the future."
+        response = client.post(url, data=data)
+        content = response.content.decode()
+
+        assert page_content.versions.first().state == DRAFT  # Error, no state change
+
+        assert (
+            f'<ul class="errorlist" id="id_visibility_start_error"><li>{expected_error}</li></ul>' in content or 
+            f'<ul class="errorlist"><li>{expected_error}</li></ul>' in content  # Django 4.2
+        )
 
     def test_publish_does_publish_without_form_data(self, client, admin_user, page_content):
-        from djangocms_versioning.constants import PUBLISHED
-
         version = page_content.versions.first()
         url = admin_reverse("djangocms_versioning_pagecontentversion_publish", args=(version.pk,))
         client.login(username=admin_user.username, password='admin123')
         client.post(url)
 
         assert page_content.versions.first().state == PUBLISHED
+
+    def test_publish_does_respect_form_data(self, client, admin_user, page_content, future_datetime, far_future_datetime):
+        version = page_content.versions.first()
+        url = admin_reverse("djangocms_versioning_pagecontentversion_publish", args=(version.pk,))
+        client.login(username=admin_user.username, password='admin123')
+        data = {
+            "visibility_start_0": future_datetime.date().isoformat(),
+            "visibility_start_1": future_datetime.strftime("%H:%M"),
+            "visibility_end_0": far_future_datetime.date().isoformat(),
+            "visibility_end_1": far_future_datetime.strftime("%H:%M"),
+        }
+        client.post(url, data=data)
+
+        version = page_content.versions.first()
+        assert version.state == PUBLISHED
+        assert hasattr(version, "visibility")
